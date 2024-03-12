41389ae16559d2c81c85b76ca516b51006970e93
RohitRathore1 <rohitrathore.imh55@gmail.com>
2024-03-12 10:09:52 +0000

    Cleanup for docker container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+source /app/.env
+
 # Wait for the database to be available
 echo "Waiting for DB to be ready..."
-while ! mariadb-admin ping -h"mysql-service" --silent; do
+until mariadb -h db -u${MARIADB_USER} -p${MARIADB_PASSWORD} -e 'SELECT 1' > /dev/null 2>&1; do
     sleep 1
+    echo "Waiting for MariaDB to become available..."
 done
+echo "MariaDB is up - initializing database if needed..."
 
 # Initialize the database
-echo "Initializing the database..."
-mariadb -h mysql-service -uadmin -padmin < /docker-entrypoint-initdb.d/init-db.sql
+if mariadb -h db -u${MARIADB_USER} -p${MARIADB_PASSWORD} ${MARIADB_DATABASE} < /docker-entrypoint-initdb.d/init-db.sql; then
+    echo "Database initialized and sample tasks inserted successfully."
+else
+    echo "Failed to initialize the database."
+    exit 1
+fi
 
-# Start the FastAPI application
+echo "Starting the application..."
 exec "$@"
